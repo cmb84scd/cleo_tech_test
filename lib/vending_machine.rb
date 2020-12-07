857067e25a091c657c36cbf5e31b55bc9f585dc9
cmb84scd <cmb84scd@gmail.com>
2020-12-07 14:54:26 +0000

    Add product selection

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -1,5 +1,16 @@
 class VendingMachine
+  attr_reader :product
+
+  def initialize
+    @product = nil
+  end
+
   def show_products
     { 'crisps' => 0.40, 'chocolate' => 0.50, 'water' => 1.00 }
+  end
+
+  def select_product
+    puts 'What product would you like?'
+    @product = gets.chomp
   end
 end
--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -1,9 +1,19 @@
 require 'vending_machine'
+require 'stringio'
 
 describe VendingMachine do
   let(:vending_machine) { VendingMachine.new }
 
   it 'shows a list of products' do
     expect(vending_machine.show_products).to include({ 'crisps' => 0.40 })
+  end
+
+  describe '#select_product' do
+    let(:input) { StringIO.new("crisps") }
+    it 'lets you select a product through user input' do
+      $stdin = input
+      expect { vending_machine.select_product }.to output("What product would you like?\n").to_stdout
+      $stdin = STDIN
+    end
   end
 end
